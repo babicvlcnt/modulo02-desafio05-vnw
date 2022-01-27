ab1cd5cf5f384aaa20f6e3649e538865308368dd
babicvlcnt <bcfrota@id.uff.br>
2022-01-27 16:35:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let cores = ["Azul", "Amarelo", "Vermelho", "Branco", "Verde"]
+console.log(cores)
+
+cores.unshift("Laranja")
+
+cores.pop()
+
+cores.push("Rosa","Cinza")
+
+cores.shift()
+
+let numbers = [7, 5, 6, 3, 8, 9, 2, 1, 4]
+console.log(numbers.sort())
